6465531fb0f2abfc9ecd2c2f3f1a75b5bf4e926e
Pedrazaax <carlos.pedraza@alu.uclm.es>
2023-07-10 16:36:05 +0200

    node_modules

--- MyHomeIQ/src/app/_components/consumo/consumo.component.ts
+++ MyHomeIQ/src/app/_components/consumo/consumo.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { Chart, initTE } from 'tw-elements';
+
+initTE({ Chart });
 
 @Component({
   selector: 'app-consumo',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./consumo.component.css']
 })
 export class ConsumoComponent {
-
+  ngOnInit() {
+    const dataBar = {
+      type: 'bar',
+      data: {
+        labels: ['Monday', 'Tuesday' , 'Wednesday' , 'Thursday' , 'Friday' , 'Saturday' , 'Sunday '],
+        datasets: [
+          {
+            label: 'Traffic',
+            data: [2112, 2343, 2545, 3423, 2365, 1985, 987],
+          },
+        ],
+      },
+    };
+  
+    new Chart(document.getElementById('bar-chart') as HTMLCanvasElement, dataBar);
+  }
 }
