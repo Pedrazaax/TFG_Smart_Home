b6768ee139bfb3ea7ed592917fc93dbb4176efa3
Pedrazaax <carlos.pedraza@alu.uclm.es>
2024-02-26 12:23:35 +0100

    apiURL en render

--- MyHomeIQ/src/app/app.config.ts
+++ MyHomeIQ/src/app/app.config.ts
@@ -1,6 +1,6 @@
 export class AppConfig {
     //http://localhost:8000
     //https://myhomeiq.onrender.com
-    apiUrl = 'http://localhost:8000';
+    apiUrl = 'https://myhomeiq.onrender.com';
 }
   
