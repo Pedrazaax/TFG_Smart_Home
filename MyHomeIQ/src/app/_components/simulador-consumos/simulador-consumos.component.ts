ef41aec65c7c978dc82a32a9b84b56e7183291e7
JoseLRC <JoseLRuizC97@gmail.com>
2024-11-11 13:42:43 +0100

    Ya se obtienen los consumos para el simulador

--- MyHomeIQ/src/app/_components/simulador-consumos/simulador-consumos.component.ts
+++ MyHomeIQ/src/app/_components/simulador-consumos/simulador-consumos.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { SimuladorDispositivo } from 'src/app/_models/prueba-consumo';
+import { ConsumoService } from 'src/app/_services/consumo.service';
 
 @Component({
     selector: 'app-simulador-consumos',
@@ -7,7 +9,25 @@ import { SimuladorDispositivo } from 'src/app/_models/prueba-consumo';
     styleUrls: ['./simulador-consumos.component.css']
 })
 export class SimuladorConsumosComponent {
+
     consumoDispositivos!: SimuladorDispositivo[]
-    constructor() {}
-    ngOnInit() {}
+
+    constructor(private consumoService: ConsumoService, private toastr: ToastrService) {
+
+    }
+    ngOnInit() {
+        this.get_dispositivosSimulador();
+    }
+
+    get_dispositivosSimulador() {
+        this.consumoService.getSimuladorDispositivos().subscribe(
+            (response: any) => {
+                this.consumoDispositivos = response
+                console.log(this.consumoDispositivos)
+            },
+            (error: any) => {
+                this.toastr.error(error.error.detail, "Error")
+            }
+        );
+    }
 }
