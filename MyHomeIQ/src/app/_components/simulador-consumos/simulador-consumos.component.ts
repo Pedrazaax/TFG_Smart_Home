823849d8200426cf3a303a8662d8c42c5ab3f3e6
JoseLRC <JoseLRuizC97@gmail.com>
2024-11-13 12:36:40 +0100

    Calculadas todas las mediciones globales

--- MyHomeIQ/src/app/_components/simulador-consumos/simulador-consumos.component.ts
+++ MyHomeIQ/src/app/_components/simulador-consumos/simulador-consumos.component.ts
@@ -11,23 +11,58 @@ import { ConsumoService } from 'src/app/_services/consumo.service';
 export class SimuladorConsumosComponent {
 
     consumoDispositivos!: SimuladorDispositivo[]
+    consumoDiario!: number
+    consumoMensual!: number
+    consumoAnual!: number
+    intensidadDiaria!: number
+    intensidadMensual!: number
+    intensidadAnual!: number
+    potenciaDiaria!: number
+    potenciaMensual!: number
+    potenciaAnual!: number
+    etiquetaGlobal!: string
 
     constructor(private consumoService: ConsumoService, private toastr: ToastrService) {
 
     }
     ngOnInit() {
         this.get_dispositivosSimulador();
+        
     }
 
     get_dispositivosSimulador() {
         this.consumoService.getSimuladorDispositivos().subscribe(
             (response: any) => {
                 this.consumoDispositivos = response
+                this.get_consumosGlobales();
                 console.log(this.consumoDispositivos)
             },
             (error: any) => {
                 this.toastr.error(error.error.detail, "Error")
             }
         );
+    }
+
+    get_consumosGlobales() {
+        let consumoGlogal = 0;
+        let intensidadGlobal = 0;
+        let potenciaGlobal = 0;
+        for (let dispositivo of this.consumoDispositivos) {
+            consumoGlogal += parseFloat(dispositivo.consumoMedio)
+            intensidadGlobal += parseFloat(dispositivo.intensidadMedia)
+            potenciaGlobal += parseFloat(dispositivo.potenciaMedia)
+        }
+        this.consumoDiario = consumoGlogal;
+        this.consumoMensual = this.consumoDiario * 31
+        this.consumoAnual = this.consumoMensual * 12
+        this.intensidadDiaria = intensidadGlobal;
+        this.intensidadMensual = this.intensidadDiaria * 31
+        this.intensidadAnual = this.intensidadMensual * 12
+        this.potenciaDiaria = potenciaGlobal
+        this.potenciaMensual = this.potenciaDiaria * 31
+        this.potenciaAnual = this.potenciaMensual * 12
+        console.log("Consumo diario: " + this.consumoDiario + " // Consumo mensual: " + this.consumoMensual + " // Consumo anual. " + this.consumoAnual)
+        console.log("Intensidad diaria: " + this.intensidadDiaria + " // Intensidad mensual: " + this.intensidadMensual + " // Intensidad anual. " + this.intensidadAnual)
+        console.log("Potencia diaria: " + this.potenciaDiaria + " // Potencia mensual: " + this.potenciaMensual + " // Potencia anual. " + this.potenciaAnual)
     }
 }
